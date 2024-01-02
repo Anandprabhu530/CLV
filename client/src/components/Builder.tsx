2e50ffacae16c80fa326fce841370b19cac17124
Anandprabhu530 <anandprabhu530@gmail.com>
2024-01-02 22:56:02 +0530

    Completed Initial objectives

--- client/src/components/Builder.tsx
+++ client/src/components/Builder.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 
 type initialstatetype = {
   firstname: string;
@@ -13,13 +14,11 @@ const Builder = () => {
     manager_name: "",
     role: "",
     company_name: "",
-    experience: null,
+    experience: Number,
     previous_role: "",
     previous_company: "",
-    description: "",
-    achievements: "",
   });
-
+  const [responsedata, setresponsedata] = useState();
   const [formerrors, setFormerrors] = useState<initialstatetype>({
     firstname: "",
     lastname: "",
@@ -59,7 +58,9 @@ const Builder = () => {
       },
       body: JSON.stringify({ prompt_data: formdata }),
     }).then((res) => res.json());
-    console.log(response);
+    console.log(response.kwargs.content);
+    setresponsedata(response.kwargs.content);
+    setIssubmit(false);
   };
 
   const handlealldata = async () => {
@@ -68,131 +69,132 @@ const Builder = () => {
   };
 
   return (
-    <div className="lg:flex lg:flex-col justify-center items-center lg:h-[90vh] md:h-[90vh] font-poppins">
-      <div className="text-white relative lg:card rounded-[1rem] md:card bg-black p-10 lg:items-center flex flex-col gap-10 border border-white">
-        <div className="font-semibold text-2xl">Personel Information</div>
-        <form action="submit" className="lg:flex lg:gap-20 md:flex">
-          <div className="grid gap-6 text-xl w-full">
-            <div>
+    <div className="lg:flex justify-center items-center lg:h-[90vh] md:h-[90vh] font-poppins gap-16">
+      <div className="basis-1/4">
+        <div className=" text-white relative lg:card rounded-[1rem] md:card bg-black p-10 lg:items-center flex flex-col gap-10 ">
+          <div className="font-semibold text-2xl">Personel Information</div>
+          <form action="submit" className="lg:flex lg:gap-20 md:flex">
+            <div className="grid gap-6 text-xl w-full">
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="First name*"
+                  type="text"
+                  name="firstname"
+                  value={formdata.firstname}
+                  className={
+                    formerrors.firstname
+                      ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
+                      : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                  }
+                />
+                <p className="text-base text-red-400">{formerrors.firstname}</p>
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Last name*"
+                  type="text"
+                  value={formdata.lastname}
+                  name="lastname"
+                  className={
+                    formerrors.lastname
+                      ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
+                      : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                  }
+                />
+                <p className="text-base text-red-400">{formerrors.lastname}</p>
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Hiring Manager Name"
+                  type="text"
+                  name="manager_name"
+                  className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                />
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Role you are applying*"
+                  type="text"
+                  name="role"
+                  value={formdata.role}
+                  className={
+                    formerrors.role
+                      ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
+                      : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                  }
+                />
+                <p className="text-base text-red-400">{formerrors.role}</p>
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Company name*"
+                  type="text"
+                  value={formdata.company_name}
+                  name="company_name"
+                  className={
+                    formerrors.company_name
+                      ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
+                      : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                  }
+                />
+                <p className="text-red-400 text-base">
+                  {formerrors.company_name}
+                </p>
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Experience in years"
+                  type="number"
+                  name="experience"
+                  className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full remove-arrow"
+                />
+              </div>
+              <div>
+                <input
+                  onChange={handlechange}
+                  placeholder="Previous Role Name"
+                  type="text"
+                  name="previous_role"
+                  className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 lg:w-[350px]"
+                />
+              </div>
               <input
                 onChange={handlechange}
-                placeholder="First name*"
+                placeholder="Previous Company Name"
                 type="text"
-                name="firstname"
-                value={formdata.firstname}
-                className={
-                  formerrors.firstname
-                    ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
-                    : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
-                }
-              />
-              <p className="text-base text-red-400">{formerrors.firstname}</p>
-            </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Last name*"
-                type="text"
-                value={formdata.lastname}
-                name="lastname"
-                className={
-                  formerrors.lastname
-                    ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
-                    : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
-                }
-              />
-              <p className="text-base text-red-400">{formerrors.lastname}</p>
-            </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Hiring Manager Name"
-                type="text"
-                name="manager_name"
-                className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full "
+                name="previous_company"
+                className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full"
               />
             </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Role you are applying*"
-                type="text"
-                name="role"
-                value={formdata.role}
-                className={
-                  formerrors.role
-                    ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
-                    : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
-                }
-              />
-              <p className="text-base text-red-400">{formerrors.role}</p>
-            </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Company name*"
-                type="text"
-                value={formdata.company_name}
-                name="company_name"
-                className={
-                  formerrors.company_name
-                    ? "border-red-400 bg-black border-b text-white placeholder:text-[#858585] outline-none py-2 w-full"
-                    : "border-b-[#858585] border-b bg-black text-white placeholder:text-[#858585] outline-none py-2 w-full "
-                }
-              />
-              <p className="text-red-400 text-base">
-                {formerrors.company_name}
-              </p>
-            </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Experience in years"
-                type="number"
-                name="experience"
-                className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full remove-arrow"
-              />
-            </div>
-            <div>
-              <input
-                onChange={handlechange}
-                placeholder="Previous Role Name"
-                type="text"
-                name="previous_role"
-                className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 lg:w-[350px]"
-              />
-            </div>
+          </form>
+          <div>
+            <button
+              type="submit"
+              onClick={handlealldata}
+              className="text-white mt-4 border border-[#858585] bg-black lg:px-4 py-2 rounded-md w-full"
+            >
+              Submit
+            </button>
           </div>
-          <div className="grid gap-6 text-xl w-full">
-            <input
-              onChange={handlechange}
-              placeholder="Previous Company Name"
-              type="text"
-              name="previous_company"
-              className="bg-black border-b border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full"
-            />
-            <textarea
-              onChange={() => handlechange}
-              placeholder="Description"
-              className="bg-black border rounded-md p-2 border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full h-[160px] resize-none lg:w-[350px] md:w-[350px]"
-              name="description"
-            />
-            <textarea
-              onChange={() => handlechange}
-              placeholder="Enter your achievements"
-              name="achievements"
-              className="bg-black border rounded-md p-2 border-[#858585] text-white placeholder:text-[#858585] outline-none py-2 w-full  h-[140px] resize-none"
-            />
-          </div>
-        </form>
-        <div>
-          <button
-            type="submit"
-            onClick={handlealldata}
-            className="text-white mt-4 border border-[#858585] bg-black lg:px-4 py-2 rounded-md w-full"
-          >
-            Submit
-          </button>
+        </div>
+      </div>
+      <div className="basis-1/2 h-full p-8">
+        <div className="h-full text-white text-base card relative bg-black rounded-xl">
+          {issubmit ? (
+            <div className="text-white text-2xl flex justify-center items-center h-full">
+              Please Wait...
+            </div>
+          ) : (
+            <ReactMarkdown className="p-8 text-lg">
+              {responsedata}
+            </ReactMarkdown>
+          )}
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -10,25 +10,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const chatModel = new ChatGoogleGenerativeAI({
-//   modelName: "gemini-pro",
-//   maxOutputTokens: 2048,
-// });
+const chatModel = new ChatGoogleGenerativeAI({
+  modelName: "gemini-pro",
+  maxOutputTokens: 2048,
+});
 
-// const prompt = ChatPromptTemplate.fromMessages([
-//   ["system", "You are a cover letter writer"],
-//   ["user", "{input}"],
-// ]);
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a cover letter writer"],
+  ["user", "{input}"],
+]);
 
-// const chain = prompt.pipe(chatModel);
+const chain = prompt.pipe(chatModel);
 
 app.post("/create", async (req, res) => {
-  console.log(req.body);
   const input_data = req.body.prompt_data;
-  // const data = await chain.invoke({
-  //   input: input_data,
-  // });
-  res.send(input_data);
+  const processed_data = `Write me a cover letter with the following details.
+  name : ${input_data.firstname} ${input_data.lastname}, 
+  role : ${input_data.role}, 
+  manager_name : ${
+    input_data.manager_name ? input_data.manager_name : "undefined"
+  }, 
+  company name i am applying=${input_data.company_name}, 
+  expreience=${input_data.experience} years, 
+  previous role : ${
+    input_data.previous_role ? input_data.previous_role : "undefined"
+  }, 
+  previous company : ${
+    input_data.previous_company ? input_data.previous_company : "undefined"
+  }, 
+  consider undefined as not applicable.Only take the given inputs.Don't generate other fields.Replace the fields with above data.Generate cover letter only.`;
+  console.log("processing");
+  const data = await chain.invoke({
+    input: processed_data,
+  });
+  res.send(data);
 });
 
 app.listen(8080, (req, res) => {
